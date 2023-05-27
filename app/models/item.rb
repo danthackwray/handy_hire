4b9854ffbca018ecce6a47220bdc714268a8521a
slosh chetty <thatawesomeindianchick@gmail.com>
2023-05-27 15:40:33 +0200

    booking_functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   validates :name, presence: true
   validates :address, presence: true
-  validates :description, presence: true
+  validates :description, presence:
 end
