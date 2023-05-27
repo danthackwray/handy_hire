c3930cb1334681603d07f06475b4ba03fdca34eb
slosh chetty <thatawesomeindianchick@gmail.com>
2023-05-27 11:58:51 +0200

    booking_functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,29 @@
 class BookingsController < ApplicationController
   def show
-    @bookings = Booking.find(params[:id])
+    @booking = Booking.find(params[:id])
     @item = Item.find(params[:item_id])
   end
 
+  def new
+    @item = Item.find(params[:item_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @booking = Booking.new(booking_params)
+    @booking.item = @item
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id)
+  end
 end
-#create bookings new
-#create bookings show
-creat emy new bookings , new and go
