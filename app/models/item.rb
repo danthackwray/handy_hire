1f319731f306787e971690deba4a4f75fa2d483b
Amanda Foster <amanda.foster@multichoice.co.za>
2023-06-02 20:41:26 +0200

    search for an item added

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,11 @@ class Item < ApplicationRecord
   validates :address, presence: true
   validates :price, presence: true
   has_many :bookings
+
+  include PgSearch::Model
+    pg_search_scope :search_by_name_and_description,
+      against: [ :name, :description],
+      using: {
+       tsearch: { prefix: true } # <-- now `superman batm` will return something!
+     }
 end
