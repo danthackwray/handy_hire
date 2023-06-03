d3b0d27843d228b8300f9afea5db696447b5cf65
phiwokuhle sibisi <phiwo.sibisi@supersport.com>
2023-06-03 15:28:01 +0200

    edit delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,10 +29,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to item_path(@item)
+  end
+
+
   def destroy
-    @item = Item.new(item_params)
+    @item = Item.find(params[:id])
     @item.destroy
-    redirect_to items_path, status: :see_other
+    redirect_to items_path, notice: "item deleted succesfully"
   end
 
   private
